a4b3961057dfa9051eafc6ed4240c872e3a33b9b
Micah Geisel <micah@botandrose.com>
2025-01-09 18:26:55 -0600

    rename two-pass tests to reflect new reality.

--- test/retain-hidden-state.js
+++ test/retain-hidden-state.js
@@ -1,7 +1,7 @@
-describe("Two-pass option for retaining more state", function () {
+describe("algorithm", function () {
   setup();
 
-  it("preserves all non-attribute element state with two-pass option", function () {
+  it("preserves all non-attribute element state", function () {
     getWorkArea().append(
       make(`
             <div>
@@ -30,7 +30,7 @@ describe("Two-pass option for retaining more state", function () {
     states.should.eql([true, true]);
   });
 
-  it("preserves all non-attribute element state with two-pass option and outerHTML morphStyle", function () {
+  it("preserves all non-attribute element state and outerHTML morphStyle", function () {
     const div = make(`
             <div>
               <input type="checkbox" id="first">
@@ -161,7 +161,7 @@ describe("Two-pass option for retaining more state", function () {
     states.should.eql([true, true]);
   });
 
-  it("preserves focus state with two-pass option and outerHTML morphStyle", function () {
+  it("preserves focus state and outerHTML morphStyle", function () {
     const div = make(`
             <div>
               <input type="text" id="first">
@@ -478,7 +478,7 @@ describe("Two-pass option for retaining more state", function () {
   });
 
   it("duplicate ids on elements aborts matching to avoid invalid morph state", function () {
-    // twoPass can try and reuse existing id's where possible and has to exclude matching on duplicate ids
+    // we try to reuse existing ids where possible and has to exclude matching on duplicate ids
     // to avoid losing content
     getWorkArea().append(
       make(`
@@ -518,8 +518,7 @@ describe("Two-pass option for retaining more state", function () {
   });
 
   it("duplicate destination ids on elements aborts matching to avoid invalid morph state", function () {
-    // twoPass can try and reuse existing id's where possible and has to exclude matching on duplicate ids
-    // to avoid losing content
+    // exclude matching on duplicate ids to avoid losing content
     getWorkArea().append(
       make(`
             <div>
@@ -555,7 +554,7 @@ describe("Two-pass option for retaining more state", function () {
     document.activeElement.outerHTML.should.equal(document.body.outerHTML);
   });
 
-  it("preserves all non-attribute element state with two-pass option and outerHTML morphStyle when morphing to two top level nodes", function () {
+  it("preserves all non-attribute element state and outerHTML morphStyle when morphing to two top level nodes", function () {
     // when using outerHTML you can replace one node with two nodes with the state preserving items split and it will just
     // pick one best node to morph and just insert the other nodes so need to check these also retain state
     const div = make(`
@@ -583,7 +582,7 @@ describe("Two-pass option for retaining more state", function () {
     states.should.eql([true, true]);
   });
 
-  it("preserves all non-attribute element state with two-pass option and outerHTML morphStyle when morphing to two top level nodes with nesting", function () {
+  it("preserves all non-attribute element state and outerHTML morphStyle when morphing to two top level nodes with nesting", function () {
     // when using outerHTML you can replace one node with two nodes with the state preserving items split and it will just
     // pick one best node to morph and just insert the other nodes so need to check these also retain state
     const div = make(`
@@ -613,7 +612,7 @@ describe("Two-pass option for retaining more state", function () {
     states.should.eql([true, true]);
   });
 
-  it("preserves all non-attribute element state with two-pass option when wrapping element changes tag", function () {
+  it("preserves all non-attribute element state when wrapping element changes tag", function () {
     // just changing the type from div to span of the wrapper causes softmatch to fail so it abandons all hope
     // of morphing and just inserts the node so we need to check this still handles preserving state here.
     const div = make(`
@@ -638,7 +637,7 @@ describe("Two-pass option for retaining more state", function () {
     states.should.eql([true]);
   });
 
-  it("preserves all non-attribute element state with two-pass option when wrapping element changes tag at top level", function () {
+  it("preserves all non-attribute element state when wrapping element changes tag at top level", function () {
     // just changing the type from div to span of the top wrapping item with outerHTML morph will cause morphOldNodeTo function
     // to morph the two nodes that don't softmatch that also needs to handle preserving state
     const div = make(`<div><input type="checkbox" id="first"></div>`);
