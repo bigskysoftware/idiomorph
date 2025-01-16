e84d94856d73370f2191b5451baaf70b39a8cb34
Micah Geisel <micah@botandrose.com>
2025-01-16 13:54:38 -0600

    normalize more function argument order and improve docs and tests.

--- test/lib/utilities.js
+++ test/lib/utilities.js
@@ -90,3 +90,38 @@ function assertNoFocus() {
   document.activeElement.tagName.should.eql("BODY");
 }
 
+function assertOps(before, after, expectedOps) {
+  let ops = [];
+  let initial = make(before);
+  let final = make(after);
+  let finalCopy = document.importNode(final, true);
+  Idiomorph.morph(initial, final, {
+    callbacks: {
+      beforeNodeMorphed: (oldNode, newNode) => {
+        // Text node morphs are mostly noise
+        if (oldNode.nodeType === Node.TEXT_NODE) return;
+
+        ops.push([
+          "Morphed",
+          oldNode.outerHTML || oldNode.textContent,
+          newNode.outerHTML || newNode.textContent,
+        ]);
+      },
+      beforeNodeRemoved: (node) => {
+        ops.push(["Removed", node.outerHTML || node.textContent]);
+      },
+      beforeNodeAdded: (node) => {
+        ops.push(["Added", node.outerHTML || node.textContent]);
+      },
+    },
+  });
+  if (JSON.stringify(ops) != JSON.stringify(expectedOps)) {
+    console.log('test expected Operations is:');
+    console.log(expectedOps);
+    console.log('test failing Operations is:');
+    console.log(ops);
+  }
+  initial.outerHTML.should.equal(finalCopy.outerHTML);
+  ops.should.eql(expectedOps);
+}
+
--- test/preserve-focus.js
+++ test/preserve-focus.js
@@ -1,25 +1,42 @@
 describe("Preserves focus where possible", function () {
   setup();
 
+  function assertFocusPreservation(
+    before,
+    after,
+    focusId,
+    selection,
+    shouldAssertFocusAndSelection = true,
+  ) {
+    getWorkArea().innerHTML = before;
+    setFocusAndSelection(focusId, selection);
+    Idiomorph.morph(getWorkArea(), after, {
+      morphStyle: "innerHTML",
+    });
+    getWorkArea().innerHTML.should.equal(after);
+    // for when we fall short of the ideal
+    // these should be considered TODOs for future improvement
+    if (shouldAssertFocusAndSelection) {
+      assertFocusAndSelection(focusId, selection);
+    }
+  }
+
   it("preserves focus state and outerHTML morphStyle", function () {
-    const div = make(`
-            <div>
-              <input type="text" id="focused" value="abc">
-              <input type="text" id="other">
-            </div>
-        `);
-    getWorkArea().append(div);
-    setFocusAndSelection("focused", "b");
-
-    let finalSrc = `
-            <div>
-              <input type="text" id="other">
-              <input type="text" id="focused" value="abc">
-            </div>
-        `;
-    Idiomorph.morph(div, finalSrc, { morphStyle: "outerHTML" });
-
-    getWorkArea().innerHTML.should.equal(finalSrc);
+    assertFocusPreservation(
+      `
+      <div>
+        <input type="text" id="focused" value="abc">
+        <input type="text" id="other">
+      </div>`,
+      `
+      <div>
+        <input type="text" id="other">
+        <input type="text" id="focused" value="abc">
+      </div>`,
+      "focused",
+      "b",
+      false, // skip assertion
+    );
     if (hasMoveBefore()) {
       assertFocus("focused");
       // TODO moveBefore loses selection on Chrome 131.0.6778.264
@@ -31,52 +48,40 @@ describe("Preserves focus where possible", function () {
   });
 
   it("preserves focus state when previous element is replaced", function () {
-    getWorkArea().innerHTML = `
-            <div>
-              <a></a>
-              <input type="text" id="focused" value="abc">
-            </div>
-        `;
-    setFocusAndSelection("focused", "b");
-
-    let finalSrc = `
-            <div>
-              <b></b>
-              <input type="text" id="focused" value="abc">
-            </div>
-        `;
-    Idiomorph.morph(getWorkArea(), finalSrc, {
-      morphStyle: "innerHTML",
-    });
-
-    getWorkArea().innerHTML.should.equal(finalSrc);
-    assertFocusAndSelection("focused", "b");
+    assertFocusPreservation(
+      `
+      <div>
+        <a></a>
+        <input type="text" id="focused" value="abc">
+      </div>`,
+      `
+      <div>
+        <b></b>
+        <input type="text" id="focused" value="abc">
+      </div>`,
+      "focused",
+      "b",
+    );
   });
 
   it("preserves focus state when elements are moved to different levels of the DOM", function () {
-    getWorkArea().append(
-      make(`
-            <div>
-              <input type="text" id="other">
-              <div>
-                <input type="text" id="focused" value="abc">
-              </div>
-            </div>
-        `),
+    assertFocusPreservation(
+      `
+      <div>
+        <input type="text" id="other">
+        <div>
+          <input type="text" id="focused" value="abc">
+        </div>
+      </div>`,
+      `
+      <div>
+        <input type="text" id="other">
+        <input type="text" id="focused" value="abc">
+      </div>`,
+      "focused",
+      "b",
+      false, // skip assertion
     );
-    setFocusAndSelection("focused", "b");
-
-    let finalSrc = `
-            <div>
-              <input type="text" id="other">
-              <input type="text" id="focused" value="abc">
-            </div>
-        `;
-    Idiomorph.morph(getWorkArea(), finalSrc, {
-      morphStyle: "innerHTML",
-    });
-
-    getWorkArea().innerHTML.should.equal(finalSrc);
     if (hasMoveBefore()) {
       assertFocus("focused");
       // TODO moveBefore loses selection on Chrome 131.0.6778.264
@@ -88,27 +93,23 @@ describe("Preserves focus where possible", function () {
   });
 
   it("preserves focus state when focused element is moved between anonymous containers", function () {
-    getWorkArea().innerHTML = `
-            <div>
-              <input type="text" id="other">
-            </div>
-            <div>
-              <input type="text" id="focused" value="abc">
-            </div>
-        `;
-    setFocusAndSelection("focused", "b");
-
-    let finalSrc = `
-            <div>
-              <input type="text" id="other">
-              <input type="text" id="focused" value="abc">
-            </div>
-        `;
-    Idiomorph.morph(getWorkArea(), finalSrc, {
-      morphStyle: "innerHTML",
-    });
-
-    getWorkArea().innerHTML.should.equal(finalSrc);
+    assertFocusPreservation(
+      `
+      <div>
+        <input type="text" id="other">
+      </div>
+      <div>
+        <input type="text" id="focused" value="abc">
+      </div>`,
+      `
+      <div>
+        <input type="text" id="other">
+        <input type="text" id="focused" value="abc">
+      </div>`,
+      "focused",
+      "b",
+      false, // skip assertion
+    );
     if (hasMoveBefore()) {
       assertFocus("focused");
       // TODO moveBefore loses selection on Chrome 131.0.6778.264
@@ -120,35 +121,29 @@ describe("Preserves focus where possible", function () {
   });
 
   it("preserves focus state when elements are moved between IDed containers", function () {
-    getWorkArea().append(
-      make(`
-            <div>
-              <div id="left">
-                <input type="text" id="focused" value="abc">
-              </div>
-              <div id="right">
-                <input type="text" id="other">
-              </div>
-            </div>
-        `),
+    assertFocusPreservation(
+      `
+      <div>
+        <div id="left">
+          <input type="text" id="focused" value="abc">
+        </div>
+        <div id="right">
+          <input type="text" id="other">
+        </div>
+      </div>`,
+      `
+      <div>
+        <div id="left">
+          <input type="text" id="other">
+        </div>
+        <div id="right">
+          <input type="text" id="focused" value="abc">
+        </div>
+      </div>`,
+      "focused",
+      "b",
+      false, // skip assertion
     );
-    setFocusAndSelection("focused", "b");
-
-    let finalSrc = `
-            <div>
-              <div id="left">
-                <input type="text" id="other">
-              </div>
-              <div id="right">
-                <input type="text" id="focused" value="abc">
-              </div>
-            </div>
-        `;
-    Idiomorph.morph(getWorkArea(), finalSrc, {
-      morphStyle: "innerHTML",
-    });
-
-    getWorkArea().innerHTML.should.equal(finalSrc);
     if (hasMoveBefore()) {
       assertFocus("focused");
       // TODO moveBefore loses selection on Chrome 131.0.6778.264
@@ -160,35 +155,29 @@ describe("Preserves focus where possible", function () {
   });
 
   it("preserves focus state when focus parent is moved down", function () {
-    getWorkArea().append(
-      make(`
-            <div>
-              <div id="with-focus">
-                <input type="text" id="focused" value="abc">
-              </div>
-              <div id="with-other">
-                <input type="text" id="other">
-              </div>
-            </div>
-        `),
+    assertFocusPreservation(
+      `
+      <div>
+        <div id="with-focus">
+          <input type="text" id="focused" value="abc">
+        </div>
+        <div id="with-other">
+          <input type="text" id="other">
+        </div>
+      </div>`,
+      `
+      <div>
+        <div id="with-other">
+          <input type="text" id="other">
+        </div>
+        <div id="with-focus">
+          <input type="text" id="focused" value="abc">
+        </div>
+      </div>`,
+      "focused",
+      "b",
+      false, // skip assertion
     );
-    setFocusAndSelection("focused", "b");
-
-    let finalSrc = `
-            <div>
-              <div id="with-other">
-                <input type="text" id="other">
-              </div>
-              <div id="with-focus">
-                <input type="text" id="focused" value="abc">
-              </div>
-            </div>
-        `;
-    Idiomorph.morph(getWorkArea(), finalSrc, {
-      morphStyle: "innerHTML",
-    });
-
-    getWorkArea().innerHTML.should.equal(finalSrc);
     if (hasMoveBefore()) {
       assertFocus("focused");
       // TODO moveBefore loses selection on Chrome 131.0.6778.264
@@ -200,35 +189,56 @@ describe("Preserves focus where possible", function () {
   });
 
   it("preserves focus state when focus parent is moved up", function () {
-    getWorkArea().append(
-      make(`
-            <div>
-              <div id="with-other">
-                <input type="text" id="other">
-              </div>
-              <div id="with-focus">
-                <input type="text" id="focused" value="abc">
-              </div>
-            </div>
-        `),
+    assertFocusPreservation(
+      `
+      <div>
+        <div id="with-other">
+          <input type="text" id="other">
+        </div>
+        <div id="with-focus">
+          <input type="text" id="focused" value="abc">
+        </div>
+      </div>`,
+      `
+      <div>
+        <div id="with-focus">
+          <input type="text" id="focused" value="abc">
+        </div>
+        <div id="with-other">
+          <input type="text" id="other">
+        </div>
+      </div>`,
+      "focused",
+      "b",
     );
-    setFocusAndSelection("focused", "b");
+  });
 
-    let finalSrc = `
-            <div>
-              <div id="with-focus">
-                <input type="text" id="focused" value="abc">
-              </div>
-              <div id="with-other">
-                <input type="text" id="other">
-              </div>
-            </div>
-        `;
-    Idiomorph.morph(getWorkArea(), finalSrc, {
-      morphStyle: "innerHTML",
-    });
-
-    getWorkArea().innerHTML.should.equal(finalSrc);
-    assertFocusAndSelection("focused", "b");
+  it("preserves focus state when matching anonymous element is inserted", function () {
+    assertFocusPreservation(
+      `
+      <div>
+        <div>
+          <input type="text" id="focused" value="abc">
+        </div>
+      </div>`,
+      `
+      <div>
+        <div></div>
+        <div>
+          <input type="text" id="focused" value="abc">
+        </div>
+      </div>`,
+      "focused",
+      "b",
+      false,
+    );
+    if (hasMoveBefore()) {
+      assertFocus("focused");
+      // TODO moveBefore loses selection on Chrome 131.0.6778.264
+      // expect will be fixed in future release
+      // assertFocusAndSelection("focused", "b");
+    } else {
+      assertNoFocus();
+    }
   });
 });
