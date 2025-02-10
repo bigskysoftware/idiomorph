8307dec0812be5141b5f71d1bd1bbdc395becdcd
Micah Geisel <micah@botandrose.com>
2025-02-09 20:34:32 -0600

    failing test to expose issue with newContent being attached while having siblings in outerHTML morph.

--- test/core.js
+++ test/core.js
@@ -561,4 +561,24 @@ describe("Core morphing tests", function () {
     Idiomorph.morph(initial.body, final.body);
     initial.body.outerHTML.should.equal(finalSrc);
   });
+
+  it("test pathlogical case of oldNode and newContent both being in the same document with siblings", function () {
+    let context = make(`
+      <div>
+        <div>hello</div>
+        <div>world</div>
+        <p>ignore me</p>
+      </div>
+    `);
+
+    let [initial, final] = context.querySelectorAll("div");
+    let ret = Idiomorph.morph(initial, final);
+    initial.outerHTML.should.equal(final.outerHTML);
+    ret.map((e) => e.outerHTML).should.eql([final.outerHTML]);
+    context.innerHTML.should.equal(`
+        <div>world</div>
+        <div>world</div>
+        <p>ignore me</p>
+      `);
+  });
 });
